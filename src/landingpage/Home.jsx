19bbf850c72dd991f1a7dd10a2acce4fb4eeb385
AkshatPatel15 <akshat15599@gmail.com>
2023-04-27 21:22:42 -0400

    home page version 1.2

--- src/landingpage/Home.jsx
+++ src/landingpage/Home.jsx
@@ -1,9 +1,17 @@
+import { Button } from "react-bootstrap";
 import "./Home.css";
 
 const Home = () => {
     return (
       <div className="HomePage">
-        hello from Home
+        <div className="text_div">
+         <span className="hi_text">Hi,</span><br></br> 
+         <span className="i_am_text">I am 
+         <span className="akshat_patel_text"> Akshat Patel</span></span><br></br>        
+           <span className="software_developer_text">Software Developer</span>
+           <div className="btn_contact"><Button variant="dark">Contact</Button></div>
+
+</div>
     </div>
       );
 }

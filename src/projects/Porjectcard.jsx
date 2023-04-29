90720f5018cb7a988bbabcc3d42107bb3bf2ba12
AkshatPatel15 <akshat15599@gmail.com>
2023-04-29 15:30:54 -0400

    added card in porjects

--- src/projects/Porjectcard.jsx
+++ src/projects/Porjectcard.jsx
@@ -1,10 +1,22 @@
+import { Button, Card, ListGroup } from "react-bootstrap";
 import "./Projects.css"
 
 
 
-function Porjectcard() {
+function Porjectcard(props) {
     return (
-      <></>
+      <div className="m-5">
+      <Card className="card-main" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.img}/>
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          {props.description}
+        </Card.Text>
+        <div className="btn_contact"><Button variant="dark" href={props.link1}>{props.link1name}</Button></div>
+        {/* <Card.Link href="#">Another Link</Card.Link> */}
+      </Card.Body>
+    </Card></div>
     );
   }
   

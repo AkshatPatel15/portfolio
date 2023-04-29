fca5220983083c0e9f8fcb6f318434999e1b91be
AkshatPatel15 <akshat15599@gmail.com>
2023-04-29 19:36:31 -0400

    added nish
    added work of nish

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,7 +1,8 @@
-// import "/Users/nishvaria/NISH/Nishportfolio/portfolio/src/about/About.css"
+import "./About.css"
 import Container from 'react-bootstrap/Container';
 // import '/Users/nishvaria/NISH/Nishportfolio/portfolio/src/static.css';
 import img2 from "../resources/about_img_2.jpg";
+
 
 // import '/Users/nishvaria/NISH/Nishportfolio/portfolio/src/static.css';
 import { IoLogoLinkedin,IoIosMail,IoIosPhonePortrait, IoLogoGithub } from "react-icons/io";

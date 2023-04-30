d24f9ae4d0e98f681304d988deb5ddb2111a4f29
AkshatPatel15 <akshat15599@gmail.com>
2023-04-30 12:35:14 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
-import Home from "./landingpage/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbarheader from "./Navbarheader";
-
 
 function App() {
   return (

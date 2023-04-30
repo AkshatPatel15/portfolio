92e5b0d4f81a5e16716af8da72fb1da84079259e
AkshatPatel15 <akshat15599@gmail.com>
2023-04-30 14:12:47 -0400

    added routing

--- src/Navbarheader.jsx
+++ src/Navbarheader.jsx
@@ -7,48 +7,27 @@ import About from './about/About';
 import Projects from './projects/Projects';
 import { useState } from 'react';
 import Home from './landingpage/Home';
+import { Link } from 'react-router-dom';
 
 const Navbarheader = () => {
 
-  const [activeTab, setActiveTab] = useState("About");
 
-  const handleSelect = (eventKey) => {
-    setActiveTab(eventKey);
-  };
-
-  let componentToRender;
-  switch (activeTab) {
-    case "About":
-      componentToRender = <About />;
-      break;
-    case "Projects":
-      componentToRender = <Projects />;
-      break;
-    case "Home":
-      componentToRender = <Home />;
-      break;
-    default:
-      break;
-  }
 
     return (
-        <>
-          <Navbar activeKey={activeTab} onSelect={handleSelect} className='navbar-custom'>
-            <Container>
-              <Navbar.Brand > Akshat Patel</Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link eventKey="Home">Home</Nav.Link>
-                <Nav.Link eventKey="About">About</Nav.Link>
-                <Nav.Link eventKey="Projects">Projects</Nav.Link>
-              </Nav>
-              <Nav>
-              <Button className='button_navbarheader' variant="dark">Contact Me</Button>
-          </Nav>
-            </Container>
-          </Navbar>
-          {componentToRender}
 
-        </>
+        <>          <Navbar className='navbar-custom'>
+        <Container>
+          <Navbar.Brand > Akshat Patel</Navbar.Brand>
+          <Nav className="me-auto">
+          <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/projects" className="nav-link">Projects</Link>
+          </Nav>
+          <Nav>
+          <Button className='button_navbarheader' variant="dark">Contact Me</Button>
+      </Nav>
+        </Container>
+      </Navbar></>
       );
 }
 

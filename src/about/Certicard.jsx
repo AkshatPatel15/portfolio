e4293a2bf6470df4ef745c8e071a5d494c30c074
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2023-05-06 18:53:22 -0400

    Merge branch 'backup' into Nish

--- src/about/Certicard.jsx
+++ src/about/Certicard.jsx
@@ -17,7 +17,7 @@ function Certicard(props) {
                     <p>{props.detail}</p>
                 </div>
                 <div className="downloadlink">
-                    <a href ={props.link}><Button>Verify Certificate</Button></a>
+                    <a href ={props.link} target="_blank"><Button>Verify Certificate</Button></a>
                 </div>
             </div>
       </div>
